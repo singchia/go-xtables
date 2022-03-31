25b7aaea88a08284f969a240eac37fcab38b7fa1
Austin Zhai <singchia@163.com>
2022-03-31 21:11:57 +0800

    add pkg

---
+++ pkg/cmd/cmd.go
@@ -0,0 +1,17 @@
+package cmd
+
+import (
+	"bytes"
+	"os/exec"
+)
+
+func Cmd(name string, arg ...string) ([]byte, []byte, error) {
+	cmd := exec.Command(name, arg...)
+	cmd.Output()
+	infoO := new(bytes.Buffer)
+	infoE := new(bytes.Buffer)
+	cmd.Stdout = infoO
+	cmd.Stderr = infoE
+	err := cmd.Run()
+	return infoO.Bytes(), infoE.Bytes(), err
+}
